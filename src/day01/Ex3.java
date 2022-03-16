c171c09637404eab8fdf1facb9dfd681c95dff0a
sh <dltngusd2@naver.com>
2022-03-16 14:26:19 +0900

    홍길동

--- src/day01/Ex3.java
+++ src/day01/Ex3.java
@@ -3,14 +3,16 @@ package day01;
 public class Ex3 {
 	public static void main(String[] args) {
 		System.out.println("=======================================");
-		System.out.println("이름	나이	전화번호		회비");
+		System.out.println("이름\t나이\t전화번호\t\t회비");
 		System.out.println("=======================================");
-		System.out.println("홍길동    \"15\"	3672-1234	\\20000");
-		System.out.println("고길동    \"15\"	2238-1234	\\30000");
-		System.out.println("김말이    \"15\"	1234-1234	\\50000");
+		System.out.println("홍길동"+1000+"ㅁㅁㅁㅁ");
+		System.out.println("홍길동"+"\t"+"\"15\"\t"+3672+"-"+1234+"\t\\20000");
+		System.out.println("고길동"+"\t"+"\"15\"\t"+2238+"-"+1234+"\t\\30000");
+		System.out.println("김말이"+"\t"+"\"15\"\t"+1234+"-"+1234+"\t\\50000");
 		System.out.println("---------------------------------------");
-		System.out.println("총합계				\\100000");
+		System.out.println("총합계\t\t\t\t"+"\\100000");
 		System.out.println("---------------------------------------");
+		System.out.println("홍길동"+"\"15\"\t"+1234+"-"+1234+"\t\\20000");
 		/*
 		 * \n : 엔터
 		 * \t : tab크키만큼 커서를 오른쪽으로 이동
